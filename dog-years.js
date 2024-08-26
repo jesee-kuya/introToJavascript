e929675ae958312da02829a3eed561cfd877b49f
jkuya <kuyajesee@proton.me>
2024-08-26 19:12:49 +0300

    Fix: Change switch case to if condition

--- dog-years.js
+++ dog-years.js
@@ -1,32 +1,24 @@
 function dogYears(n, age) {
     let years = age / 31557600 * 7
     let num
+    
 
-    switch (n) {
-        case 'earth':
+    if (n === 'earth') {
             num = years;
-            break
-        case 'mercury':
+    }else if (n === 'mercury') {
             num = years / 0.2408467;
-            break
-        case 'venus':
+    }else if (n ==='venus'){
             num = years / 0.61519726;
-            break
-        case 'mars' : 
-            num = years / 1.8808158
-            break
-        case 'jupiter' : 
-            num = years / 11.862615
-            break
-        case 'saturn' : 
-            num = years / 29.447498
-            break
-        case 'uranus' :
-            num = years / 84.016846 
-            break
-        case 'neptune' :
-            num = years / 164.79132
-            break
+    }else if (n === 'mars') {
+            num = years / 1.8808158;
+    }else if (n === 'jupiter') {
+            num = years / 11.862615;
+    }else if (n === 'saturn') {
+            num = years / 29.447498;
+    }else if (n === 'uranus') {
+            num = years / 84.016846;
+    }else if (n === 'neptune') {
+            num = years / 164.79132;
     }
     return Number(num.toFixed(2))
 }
