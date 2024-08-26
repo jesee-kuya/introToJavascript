08c96d502bcf2d9ff4bdd9a706f1f5c7ad49afd9
jkuya <kuyajesee@proton.me>
2024-08-26 15:01:36 +0300

    Fix: Escape special characters

--- declarations.js
+++ declarations.js
@@ -1,10 +1,10 @@
-const escapeStr = "\`\\/\"'";
+const escapeStr = "\`\\\/\"\'";
 const arr = [4,2];
-const obj = {str : "jesee", num : 0, bool : true, undef : undefined};
+const obj = {str : "", num : 0, bool : true, undef : undefined};
 const nested = {
     arr :[4, undefined, '2'],
     obj : {
-        str: "jesee",
+        str: "",
         num: 0,
         bool: true,
     },
@@ -13,5 +13,5 @@ const nested = {
 Object.freeze(arr);
 Object.freeze(obj);
 Object.freeze(nested);
-Object.freeze(nested.obj);
 Object.freeze(nested.arr);
+Object.freeze(nested.obj);
