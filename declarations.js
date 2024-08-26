639155a022ef7d6f1d0c47a8e888aab07f488f47
jkuya <kuyajesee@proton.me>
2024-08-26 14:50:33 +0300

    Fix: End the statements with a ;

--- declarations.js
+++ declarations.js
@@ -1,18 +1,17 @@
-const escapeStr = "`\\/\"'"
-const arr = [4,2]
-const obj = {str : "jesee", num : 0, bool : 1 == 1, undef : undefined}
+const escapeStr = "`\\/\"'";
+const arr = [4,2];
+const obj = {str : "jesee", num : 0, bool : 1 == 1, undef : undefined};
 const nested = {
     arr :[4, undefined, '2'],
     obj : {
         str: "jesee",
         num: 0,
         bool: true,
-    }
+    },
+};
 
-}
-
-Object.freeze(arr)
-Object.freeze(obj)
-Object.freeze(nested)
-Object.freeze(nested.obj)
-Object.freeze(nested.arr)
+Object.freeze(arr);
+Object.freeze(obj);
+Object.freeze(nested);
+Object.freeze(nested.obj);
+Object.freeze(nested.arr);
