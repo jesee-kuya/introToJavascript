2e196d05b5c5b81cd986827244a5c6243006f20d
jkuya <kuyajesee@proton.me>
2024-08-27 17:23:21 +0300

    Feat: Declare function to check for value types

--- is.js
+++ is.js
@@ -1,22 +1,11 @@
-// is.num =function(n) {
-//     if (typeof n == Number) {
-//         return true
-//     }else {
-//         return false
-//     }
-// }
-
 is.num = (n) => typeof n === "number";
-is.nan = (n) => typeof n === "Nan";
+is.nan = (n) => Number.isNaN(n);
 is.str = (n) => typeof n === "string";
 is.bool = (n) => typeof n === "boolean";
-is.undef = (n) => typeof n == "undefined";
-is.def = (n) => typeof n == "defined"
-is.arr = (n) => typeof n == "array"
-is.obj = (n) => typeof n == "object"
-is.fun = (n) => typeof n == "function"
-is.truthy = (n) => typeof n == "truthy"
-is.falsy = (n) => typeof n == "falsy"
-
-
-
+is.undef = (n) => typeof n === "undefined";
+is.def = (n) => typeof n !== "undefined";
+is.arr = (n) => Array.isArray(n);
+is.obj = (n) => typeof n === "object";
+is.fun = (n) => typeof n === "function";
+is.truthy = (n) => typeof n === Boolean(n);
+is.falsy = (n) => typeof n === !Boolean(n);
