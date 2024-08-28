3a514576fe39ee0d1f233a5b01f519a08a0c1148
jkuya <kuyajesee@proton.me>
2024-08-28 18:16:10 +0300

    Feat: Declare functions that handle data types

---
+++ collections.js
@@ -0,0 +1,24 @@
+const arrToSet = (n) => new Set(n);
+const arrToStr = (n) => n.toString();
+const setToArr = (n) => Array.from(n);
+const setToStr = (n) => setToArr(n).toString();
+const strToArr = (n) => [...n];
+const strToSet = (n) => arrToSet(strToArr(n));
+const mapToObj = (n) => Object.fromEntries(n.entries());
+const objToArr = (n) => Object.keys(n);
+const objToMap = (n) => new Map(Object.entries(n));
+const arrToObj = (n) => Object.assign({}, n);
+const strToObj = (n) => arrToObj(strToArr(n));
+function superTypeOf(n) {
+    if (n instanceof Set){
+        return 'Set';
+    }else if (n instanceof Map) {
+        return 'Map';
+    }else if (n instanceof Array) {
+        return 'Array';
+    }else if (n === null) {
+        return 'null';
+    }else {
+        return typeof n;
+    }
+}
