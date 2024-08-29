1079b7e96c37f47fd5cb15b1330c8e9f6e6bd455
jkuya <kuyajesee@proton.me>
2024-08-29 18:03:28 +0300

    Feat: Declare functions that multiply, divide and modulo two numbers

---
+++ elementary.js
@@ -0,0 +1,35 @@
+function multiply(a,b) {
+    let num = 0
+    while (b !== 0) {
+        num += a
+        b--
+    }
+    return num
+}
+
+function divide(a,b) {
+    if (a <= 0 || b <= 0) {
+        return 0
+    }
+    let num = 0
+    let count = 0
+
+    while (num !== a) {
+        num += b
+        count++
+    }
+    return count
+}
+
+function modulo(a,b) {
+    if (a <= 0 || b <= 0) {
+        return 0
+    }
+    let num = 0
+
+    while (num < a) {
+        num += b
+    }
+    num -= b
+    return a - num
+}
