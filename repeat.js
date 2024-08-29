d368894b5e46e431952eb3e5f1e86e6bf54c15f2
jkuya <kuyajesee@proton.me>
2024-08-29 17:35:27 +0300

    Fix: don't add space when concatinating the string

--- repeat.js
+++ repeat.js
@@ -1,11 +1,7 @@
 function repeat(str, n) {
     let w = ''
     while (n !== 0) {
-        if (w === '') {
-            w += str
-        }else {
-            w = w + ' ' + str
-        }
+        w += str
         n--
     }
     return w
