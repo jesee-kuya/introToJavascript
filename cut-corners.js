36d9215b8b871962c74d56cfd36a8ef9235c95da
jkuya <kuyajesee@proton.me>
2024-08-30 19:14:04 +0300

    Fix: try a different approach

--- cut-corners.js
+++ cut-corners.js
@@ -1,68 +1,86 @@
-const toNum = (n) => {
-    let num = Math.abs(n)
-    let count = 0
-
-    while (num >= 1) {
-        num -= 1
-        count += 1
+function round(int) {
+    let neg = false;
+    if (int < 0) {
+        neg = true;
+        int = -int;
     }
-    return count
+    let counter = 0;
+    while (!(int < 1 && int > -1)) {
+        int -= 1;
+        counter++;
+    }
+    if (int < 0.5) {
+        if (neg) {
+            return -counter;
+        } else {
+            return counter;
+        }
+    } else {
+        if (neg) {
+            return -counter - 1;
+        } else {
+            return counter + 1;
+        }
+    }
 }
 
-function round(n) {
-    let num = toNum(n)
-    let neg = false
-
-    if (n < 0) {
-        n = -n
-        neg = true
+function floor(int) {
+    let neg = false;
+    if (int < 0) {
+        neg = true;
+        int = -int;
     }
-
-    let point = n - num
-
-    if (point >= 0.5) {
-        num += 1
+    let intCopy = int;
+    let counter = 0;
+    while (!(intCopy < 1 && intCopy > -1)) {
+        intCopy -= 1;
+        counter++;
     }
-    return neg ? -num : num
+    if (neg) {
+        return -counter - 1;
+    } else {
+        return counter;
+    }
 }
 
-function ceil(n) {
-    if (n === 0) {
-        return 0
+function ceil(int) {
+    if (!int) return 0;
+    let neg = false;
+    if (int < 0) {
+        neg = true;
+        int = -int;
     }
-    let num = toNum(n)
-    let neg = false
-
-    if (n < 0) {
-        n = -n
-        neg = true
+    let intCopy = int;
+    let counter = 0;
+    while (!(intCopy < 1 && intCopy >= 0)) {
+        intCopy -= 1;
+        counter++;
     }
-
-    return neg? -num : num + 1
+    if (neg) {
+        return -counter;
+    } else {
+        return counter + 1;
+    }
 }
 
-function floor(n) {
-    let num = toNum(n)
-    let neg = false
-
-    if (n < 0) {
-        n = -n
-        neg = true
+function trunc(int) {
+    let counter = 0;
+    if (int > 0xfffffffff) {
+        int -= 0xfffffffff;
+        counter += 0xfffffffff;
     }
-
-    return neg? -(num + 1) : num
-}
-
-function trunc(n) {
-    let num = toNum(n)
-    let neg = false
-
-    if (n < 0) {
-        neg = true
+    let neg = false;
+    if (int < 0) {
+        neg = true;
+        int = -int;
     }
-    return neg? -num : num
+    let intCopy = int;
+    while (!(intCopy < 1 && intCopy > -1)) {
+        intCopy -= 1;
+        counter++;
+    }
+    if (neg) {
+        return -counter;
+    }
+    return counter;
 }
-
-
-
-
