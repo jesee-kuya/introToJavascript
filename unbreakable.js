d65b862adaf3a6d8a09202850b907a917573d8d8
jkuya <kuyajesee@proton.me>
2024-08-30 19:42:31 +0300

    Feat: Declare functions that act like Array.join and String.split

--- unbreakable.js
+++ unbreakable.js
@@ -1,42 +1,34 @@
-function split(str, sep) {
-    let arr = []
-    let s = ''
-    if (sep !== '') {
-        for (let i = 0; i < str.length; i++) {
-            if (str[i] !== sep) {
-                s += str[i]
-            }else {
-                if (s !== '') {
-                    arr.push(s)
-                    s = ''
-                }
-            }
-        }
-        if (s !== '') {
-            arr.push(s)
-            s = ''
-        }
-    }else {
-        for (let i = 0; i < str.length; i++) {
-            arr.push(str[i])
-        }
+function join(arr, sep) {
+    if (sep === null) {
+        sep = ",";
     }
-    return arr
+    var result = arr[0].toString();
+    for (var i = 1; i < arr.length; i++) {
+        result += sep + arr[i];
+    }
+    return result;
 }
 
-
-function join(arr, joiner) {
-    let str = ''
-    for (let i = 0 ; i < arr.length; i++) {
-        if (str !== '') {
-            if (joiner !== undefined) {
-                str = str + joiner + arr[i]
-            }else {
-                str += arr[i]
-            }
-        }else {
-            str += arr[i]
-        }
+function split(str, sep) {
+    if (sep === null) {
+        sep = ",";
     }
-    return str
+    var result = [];
+    if (sep === "") {
+        for (var i = 0; i < str.length; i++) {
+            result.push(str[i]);
+        }
+        return result;
+    }
+    var end = str.indexOf(sep);
+    while (end > -1) {
+        end = str.indexOf(sep);
+        if (end === -1) {
+            break;
+        }
+        result.push(str.slice(0, end));
+        str = str.slice(end + sep.length);
+    }
+    result.push(str);
+    return result;
 }
