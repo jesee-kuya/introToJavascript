11912bbe1b586430fecdb7ead0513258b5b76baf
jkuya <kuyajesee@proton.me>
2024-08-31 14:24:57 +0300

    FIx: check for undefined and null variables

--- same-amount.js
+++ same-amount.js
@@ -1,11 +1,16 @@
 function sameAmount(str, reg1, reg2) {
+    if (str === undefined || str === null ) {
+        return false
+    }
     let re1 = new RegExp(reg1, "g");
     let re2 = new RegExp(reg2, "g");
 
-    let str1 = str.match(re1);
-    let str2 = str.match(re2);
+    let str1 = str.match(re1) || [];
+    let str2 = str.match(re2) || [];
 
     return str1.length === str2.length
 }
 
 // console.log(sameAmount('hello how are you', /l/, /l/))
+let data = null
+console.log(sameAmount(data, /h/, /w/))
