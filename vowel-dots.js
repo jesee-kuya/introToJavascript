c76e0f22dee090efd8f9c23a62106e880739aeb6
jkuya <kuyajesee@proton.me>
2024-08-31 14:02:49 +0300

    Fix: Declare the variable vowels outside the function

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,5 +1,7 @@
-function vowelDots(str) {
-    const vowels = /[aeiouAEIOU]/;
+const vowels = /[aeiou]/gi;
 
+function vowelDots(str) {
     return str.replace(vowels, (match) => match + '.')
 }
+
+console.log(vowelDots('abcdeIfghIk'))
