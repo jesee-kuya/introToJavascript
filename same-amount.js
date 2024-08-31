ac148f7f157c85325601d4b60b70c4441d59a482
jkuya <kuyajesee@proton.me>
2024-08-31 14:21:22 +0300

    Fix: check if the lenght of the matches are the same

--- same-amount.js
+++ same-amount.js
@@ -1,12 +1,11 @@
 function sameAmount(str, reg1, reg2) {
-    let re1 = new RegExp(reg1, "gi");
-    let re2 = new RegExp(reg2, "gi");
+    let re1 = new RegExp(reg1, "g");
+    let re2 = new RegExp(reg2, "g");
 
     let str1 = str.match(re1);
     let str2 = str.match(re2);
 
-    let len1 = (str1) => str1.length === 0? 0 : str1.length;
-    let len2 = (str2) => str2.length === 0? 0 : str2.length;
-
-    return len1 === len2
+    return str1.length === str2.length
 }
+
+// console.log(sameAmount('hello how are you', /l/, /l/))
