e8b35f0027dfc111cf111bcdaaa6ce8ab0491744
jkuya <kuyajesee@proton.me>
2024-09-02 19:03:48 +0300

    Feat: declare 5 functions to deal with date

---
+++ date-is.js
@@ -0,0 +1,19 @@
+const isValid = (date) => {
+    return isValid(date)
+}
+
+const isAfter = (date, date1) => {
+    return isAfter(date1, date)
+}
+
+const isBefore = (date, date1) => {
+    return isBefore(date1, date)
+}
+
+const isFuture = (date) => {
+    return isFuture(date)
+}
+
+const isPast = (date) => {
+    return isPast(date)
+}
