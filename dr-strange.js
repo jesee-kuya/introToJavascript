57b1553551d25ae3ce783423c856ce89cf7b6bd2
jkuya <kuyajesee@proton.me>
2024-09-02 18:44:37 +0300

    Fix: Map all the days and use epoch time along side 86400000

--- dr-strange.js
+++ dr-strange.js
@@ -1,12 +1,22 @@
-const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-
-const addWeek = (date) => {
-    let day = date.getDate();
-    let i = date.getDay();
-    if (day % 14 > 7 || day % 14 === 0) {
-        return 'second' + dayNames[i];
-    }
-    return dayNames[i];
+function addWeek(date) {
+    const secondWeek = {
+        0: "Monday",
+        1: "Tuesday",
+        2: "Wednesday",
+        3: "Thursday",
+        4: "Friday",
+        5: "Saturday",
+        6: "Sunday",
+        7: "secondMonday",
+        8: "secondTuesday",
+        9: "secondWednesday",
+        10: "secondThursday",
+        11: "secondFriday",
+        12: "secondSaturday",
+        13: "secondSunday",
+    };
+    let zTime = date.getTime() + 62135596800000;
+    return secondWeek[(zTime / 86400000) % 14];
 }
 
 
@@ -19,10 +29,3 @@ const timeTravel = (obj) => {
     let sec = obj.second;
     return new Date(String(yr) + '-' +String(mn) + '-' + String(dt) + ' '  + hr + ':' + min + ':'+ sec);
 }
-
-// console.log(addWeek(new Date('0001-01-01')))
-// console.log(addWeek(new Date('0001-01-02')))
-// console.log(addWeek(new Date('0001-01-07')) )
-// console.log(addWeek(new Date('0001-01-08')))
-// console.log(addWeek(new Date('0001-01-09')) )
-// console.log(addWeek(new Date('0001-01-14')))
