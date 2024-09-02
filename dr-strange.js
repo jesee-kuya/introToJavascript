bf719ec5e6e38251f2ff36ec97d2f826e205f427
jkuya <kuyajesee@proton.me>
2024-09-02 19:17:58 +0300

    Feat: Refactor: change the secondWeek object to an array

--- dr-strange.js
+++ dr-strange.js
@@ -1,20 +1,20 @@
 function addWeek(date) {
-    const secondWeek = {
-        0: "Monday",
-        1: "Tuesday",
-        2: "Wednesday",
-        3: "Thursday",
-        4: "Friday",
-        5: "Saturday",
-        6: "Sunday",
-        7: "secondMonday",
-        8: "secondTuesday",
-        9: "secondWednesday",
-        10: "secondThursday",
-        11: "secondFriday",
-        12: "secondSaturday",
-        13: "secondSunday",
-    };
+    const secondWeek = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+        "secondMonday",
+        "secondTuesday",
+        "secondWednesday",
+        "secondThursday",
+        "secondFriday",
+        "secondSaturday",
+        "secondSunday",
+    ];
     let zTime = date.getTime() + 62135596800000;
     return secondWeek[(zTime / 86400000) % 14];
 }
@@ -30,3 +30,5 @@ const timeTravel = (obj) => {
     let sec = obj.second;
     return new Date(String(yr) + '-' +String(mn) + '-' + String(dt) + ' '  + hr + ':' + min + ':'+ sec);
 }
+
+console.log(addWeek(new Date('0001-01-01')),addWeek(new Date('0001-01-02')), addWeek(new Date('0001-01-07')), addWeek(new Date('0001-01-08')), addWeek(new Date('0001-01-09')));
