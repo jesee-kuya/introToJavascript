c98370373a30c8f397c255df70b3bbb4877ea5f7
jkuya <kuyajesee@proton.me>
2024-09-03 14:16:15 +0300

    Fix: declare functions using function and not const

--- date-is.js
+++ date-is.js
@@ -1,21 +1,21 @@
-const isValid = (date) => {
+function isValid(date) {
     let n = new Date(date)
     return String(n.valueOf()) !== 'NaN'
 }
 
-const isAfter = (date, date1) => {
+function isAfter(date, date1){
     let d1 = new Date(date).getTime()
     let d2 = new Date(date1).getTime()
     return d1 > d2
 }
 
-const isBefore = (date, date1) => {
+function isBefore(date, date1) {
     let d1 = new Date(date).getTime()
     let d2 = new Date(date1).getTime()
     return d1 < d2
 }
 
-const isFuture = (date) => {
+function isFuture(date) {
     let n = new Date(date)
     if (String(n.valueOf()) === 'NaN') {
         return false
@@ -23,7 +23,7 @@ const isFuture = (date) => {
     return new Date().getTime() < new Date(date).getTime()
 }
 
-const isPast = (date) => {
+function isPast(date) {
     let n = new Date(date)
     if (String(n.valueOf()) === 'NaN') {
         return false
