a4a65ff35df2cb17e703b173365b9e4c87a090ea
jkuya <kuyajesee@proton.me>
2024-09-03 12:40:09 +0300

    Feat: Write your own functions that act like isValid, isAfter, isBefore, isFuture and isPast

--- date-is.js
+++ date-is.js
@@ -1,19 +1,25 @@
 const isValid = (date) => {
-    return isValid(date)
+    let n = new Date(date)
+    return String(n.valueOf()) !== 'NaN'
 }
 
 const isAfter = (date, date1) => {
-    return isAfter(date1, date)
+    let d1 = new Date(date).getTime()
+    let d2 = new Date(date1).getTime()
+    return d1 > d2
 }
 
 const isBefore = (date, date1) => {
-    return isBefore(date1, date)
+    return !isAfter(date, date1)
 }
 
 const isFuture = (date) => {
-    return isFuture(date)
+    if (!isValid(date)) {
+        return false
+    }
+    return new Date().getTime() < new Date(date).getTime()
 }
 
 const isPast = (date) => {
-    return isPast(date)
+    return !isFuture(date)
 }
