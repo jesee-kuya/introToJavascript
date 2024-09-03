d9746c114707bd978b098dc2f1bbda6ac3ec1437
jkuya <kuyajesee@proton.me>
2024-09-03 19:33:13 +0300

    Fix: use get full year to get the years and not get year to get the year munus 1900

--- count-leap-years.js
+++ count-leap-years.js
@@ -1,10 +1,12 @@
 const countLeapYears = (date) => {
     let yrs = 0
 
-    for (let i = 1; i <= date.getYear(); i++) {
+    for (let i = 1; i < date.getFullYear(); i++) {
         if (i % 4 === 0 && (i % 100 !== 0 || i % 400 === 0)) {
             yrs++
         }
     }
     return yrs
 }
+
+console.log(countLeapYears(new Date('1664-08-09')))
