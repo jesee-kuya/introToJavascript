016df8f0074f17e8df9ea42cf66c7229c21c36a3
jkuya <kuyajesee@proton.me>
2024-09-03 12:44:42 +0300

    Fix: Do not call the functions anywhere in the code

--- date-is.js
+++ date-is.js
@@ -10,16 +10,23 @@ const isAfter = (date, date1) => {
 }
 
 const isBefore = (date, date1) => {
-    return !isAfter(date, date1)
+    let d1 = new Date(date).getTime()
+    let d2 = new Date(date1).getTime()
+    return d1 < d2
 }
 
 const isFuture = (date) => {
-    if (!isValid(date)) {
+    let n = new Date(date)
+    if (String(n.valueOf()) === 'NaN') {
         return false
     }
     return new Date().getTime() < new Date(date).getTime()
 }
 
 const isPast = (date) => {
-    return !isFuture(date)
+    let n = new Date(date)
+    if (String(n.valueOf()) === 'NaN') {
+        return false
+    }
+    return new Date().getTime() > new Date(date).getTime()
 }
