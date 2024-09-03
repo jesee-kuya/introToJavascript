a2616d24ea66ae192ceb5a6ad1d827b3da3ba87b
jkuya <kuyajesee@proton.me>
2024-09-03 19:17:30 +0300

    Feat: declare a function that counts leap years

---
+++ count-leap-years.js
@@ -0,0 +1,10 @@
+const countLeapYears = (date) => {
+    let yrs = 0
+
+    for (let i = 1; i <= date.getYears(); i++) {
+        if (i % 4 === 0 ) {
+            yrs++
+        }
+    }
+    return yrs
+}
