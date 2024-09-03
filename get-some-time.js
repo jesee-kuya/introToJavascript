df1775ad673094c59b570932bb3e61e1a3fb3f7e
jkuya <kuyajesee@proton.me>
2024-09-03 15:52:59 +0300

    Feat: return the date as a string

--- get-some-time.js
+++ get-some-time.js
@@ -1,6 +1,8 @@
 const firstDayWeek = (num, str) => {
-    let day = new Date(String(num) +',' + str)
-    return day.getDay();
+    let week = 86400000 * num
+    let date = new Date('01-01-'+str)
+    let n = date.getTime() + week - 86400000
+    return (new Date(n)).toLocaleDateString()
 }
 
-console.log(firstDayWeek(19, '1975'))
+console.log(firstDayWeek(1, '1000'))
