62f6393bfd73b9ef823e37d914eb0f1afdb2061f
jkuya <kuyajesee@proton.me>
2024-09-03 15:58:02 +0300

    Feat: Declare a function changeFormat that changes the format of date

--- get-some-time.js
+++ get-some-time.js
@@ -2,7 +2,16 @@ const firstDayWeek = (num, str) => {
     let week = 86400000 * num
     let date = new Date('01-01-'+str)
     let n = date.getTime() + week - 86400000
-    return (new Date(n)).toLocaleDateString()
+    return changeFormat((new Date(n)).toLocaleDateString())
 }
 
+const changeFormat = (date) => {
+    let str = date.split('/')
+    for (let i = 0 ; i < str.length; i++) {
+        if (str[i].length === 1) {
+            str[i] = '0' + str[i]
+        }
+    }
+    return str.join('-')
+}
 console.log(firstDayWeek(1, '1000'))
