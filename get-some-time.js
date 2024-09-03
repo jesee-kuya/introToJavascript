76643ba5212742156236631f01b57dc103fc27aa
jkuya <kuyajesee@proton.me>
2024-09-03 16:32:12 +0300

    Fix: ensure you are getting the milliseconds of the weeks correctly

--- get-some-time.js
+++ get-some-time.js
@@ -1,17 +1,25 @@
 const firstDayWeek = (num, str) => {
-    let week = 86400000 * num
-    let date = new Date('01-01-'+str)
-    let n = date.getTime() + week - 86400000
+    let week = (86400000 * num) * 7
+    let date = new Date(str)
+    let n = (date.getTime() + week) - (86400000 * 7)
+    while ((new Date(n).getDay()) !== 1) {
+        n -= 86400000 
+    }
     return changeFormat((new Date(n)).toLocaleDateString())
 }
 
 const changeFormat = (date) => {
     let str = date.split('/')
+    let l = str[0]
+    let h = str[1]
+
+    str[1] = l
+    str[0] = h
     for (let i = 0 ; i < str.length; i++) {
+        str[0], str[1] = str[1], str[0]
         if (str[i].length === 1) {
             str[i] = '0' + str[i]
         }
     }
     return str.join('-')
 }
-console.log(firstDayWeek(1, '1000'))
