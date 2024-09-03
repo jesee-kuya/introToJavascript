91607c50e2f8dbda9682bdd2ead035acfa7e411b
jkuya <kuyajesee@proton.me>
2024-09-03 19:28:13 +0300

    Fix: check if i is divisble by 100

--- count-leap-years.js
+++ count-leap-years.js
@@ -2,7 +2,7 @@ const countLeapYears = (date) => {
     let yrs = 0
 
     for (let i = 1; i <= date.getYear(); i++) {
-        if (i % 4 === 0 ) {
+        if (i % 4 === 0 && (i % 100 !== 0 || i % 400 === 0)) {
             yrs++
         }
     }
