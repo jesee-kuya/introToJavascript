686fd5a561a6b3b78e4ba06559f8a922566b3ccc
jkuya <kuyajesee@proton.me>
2024-09-04 15:18:03 +0300

    Feat: declare a function trimTemp that takes an array of objects and returns a new array of the same structure but with spaces in temperature removed

--- using-map.js
+++ using-map.js
@@ -1,36 +1,48 @@
 const citiesOnly = (arr) => {
-    let a = []
+    let a = [];
     let arr1 = arr.map((x) => {
-        a.push(x.city)
-    })
-    return a
+        a.push(x.city);
+    });
+    return a;
 }
 
 const upperCasingStates = (arr) => {
     let arr1 = arr.map((x) => {
-        let nArr = x.split('')
-        nArr[0] = nArr[0].toUpperCase()
+        let nArr = x.split('');
+        nArr[0] = nArr[0].toUpperCase();
         for (let i = 0; i < nArr.length; i++) {
             if (nArr[i] === ' ') {
-                nArr[i + 1] = nArr[i + 1].toUpperCase()
-            }
-        }
-        return nArr.join('')
-    })
-    return arr1
+                nArr[i + 1] = nArr[i + 1].toUpperCase();
+            };
+        };
+        return nArr.join('');
+    });
+    return arr1;
 }
 
 const fahrenheitToCelsius = (arr) => {
     let arr1 = arr.map((x) => {
-        let n = ''
+        let n = '';
         for (let i = 0; i < x.length; i++) {
             if (x[i] >= '0' && x[i] <= '9') {
-                n += x[i]
-            }
-        }
-        return String(Math.round((Number(n) - 32) * (5/9))) + '°C'
+                n += x[i];
+            };
+        };
+        return String(Math.round((Number(n) - 32) * (5/9))) + '°C';
     })
-    return arr1
+    return arr1;
 }
 
-console.log(fahrenheitToCelsius(['68°F', '59°F', '25°F']))
+const trimTemp = (arr) => {
+    let arr1 = arr.map((x) => {
+        let tem = x.temperature.split(' ');
+        x.temperature = tem.join('');
+        return x;
+    })
+    return arr1;
+}
+
+console.log(trimTemp([
+    { city: 'Los Angeles', temperature: '  101 °F   ' },
+    { city: 'San Francisco', temperature: ' 84 ° F   ' },
+  ]))
