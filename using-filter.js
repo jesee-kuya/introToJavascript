818528a541017cc8daa2e90ddb57ee377a5cc579
jkuya <kuyajesee@proton.me>
2024-09-04 16:47:27 +0300

    Feat: declare a function filterStartVowel that takes an array and return only the strings that start with vowels

--- using-filter.js
+++ using-filter.js
@@ -1,5 +1,12 @@
 const filterShortStateName = (arr) => {
     return arr.filter((x) => x.length < 7)
 }
+
+const filterStartVowel = (arr) => {
+    return arr.filter((x) => (x[0] === 'a' || x[0] === 'e'|| x[0] ==='i'|| x[0] ==='o'|| x[0] ==='u'))
+}
+
+
+
 const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
-console.log(filterShortStateName(words))
+console.log(filterStartVowel(words))
