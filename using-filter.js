bdae630b9f0dfb61994b9a5abea56e9c016dbb8c
jkuya <kuyajesee@proton.me>
2024-09-04 16:55:18 +0300

    Feat: Declare a function that returns words with atleast 5 vowels

--- using-filter.js
+++ using-filter.js
@@ -6,7 +6,17 @@ const filterStartVowel = (arr) => {
     return arr.filter((x) => (x[0] === 'a' || x[0] === 'e'|| x[0] ==='i'|| x[0] ==='o'|| x[0] ==='u'))
 }
 
-
-
-const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
-console.log(filterStartVowel(words))
+const filter5Vowels = (arr) => {
+    let arr1 = arr.filter((x) => {
+        let n = 0
+        for (let i = 0; i < x.length; i++) {
+            if (x[i] === 'a' || x[i] === 'e'|| x[i] ==='i'|| x[i] ==='o'|| x[i] ==='u') {
+                n++
+            }
+        }
+        return n >= 5 ? x : ''
+    })
+    return arr1
+}
+const words = ['spray', 'elite', 'exuberanta', 'destruction', 'present', "Alabama"];
+console.log(filter5Vowels(words))
