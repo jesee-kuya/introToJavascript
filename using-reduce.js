ffa3c86c474e6885546592cf02b38a188cf03642
jkuya <kuyajesee@proton.me>
2024-09-04 19:33:07 +0300

    Feat: declare a function that adds the elements of an array if the element is odd and multiplies the element if it is even

--- using-reduce.js
+++ using-reduce.js
@@ -1,7 +1,28 @@
-const adder = (arr) => {
-    let arr1 = arr.reduce((accumulater, value) => accumulater+value)
+const adder = (arr, initial) => arr.reduce((accumulater, value) => accumulater+value,initial)
+
+const sumOrMul = (arr, initial) => {
+    let arr1 = arr.reduce((accumulater, value) => {
+        if ((value % 2 === 0 && value !== 1)){
+            if (initial !== undefined) {
+                accumulater = initial
+                initial = undefined
+            }
+            accumulater *= value
+        } else {
+            if (initial !== undefined) {
+                accumulater = initial
+                initial = undefined
+            }
+            accumulater += value
+        }
+        return accumulater   
+    })
     return arr1
 }
 
-const array1 = [1, 2, 3, 4];
-console.log(adder(array1))
+
+const funcExec = (arr) => {
+    let arr1 = arr.reduce((accumulater, value) => accumulater + value)
+}
+const array1 = [0,1, 2, 3, 4];
+console.log(sumOrMul(array1, 5))
