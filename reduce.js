477ac6a0f6b46aae47b3926e667b7c8d69988bd1
jkuya <kuyajesee@proton.me>
2024-09-05 17:50:34 +0300

    Feat: declare a function foldRight that accumulates elements of an array starting from the right

--- reduce.js
+++ reduce.js
@@ -8,3 +8,14 @@ const fold = (arr, func, accumulator) => {
     }
     return n
 }
+
+const foldRight = (arr, func, accumulator) => {
+    let n = 0
+    if (accumulator !== undefined) {
+        n = accumulator
+    }
+    for (let i = arr.length - 1; i >= 0 ; i--) {
+        n += (func(arr[i], i, arr))
+    }
+    return n
+}
