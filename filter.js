2e3e60860bfe3ce9cabd8405811709223fe8301e
jkuya <kuyajesee@proton.me>
2024-09-05 17:09:08 +0300

    Feat: declare a function that works like the reject function from lodash

--- filter.js
+++ filter.js
@@ -1,9 +1,19 @@
 const filter = (arr, func) => {
     let arr1 = []
     for (let i = 0 ; i < arr.length; i++) {
-        if (func(arr[i, i, arr])) {
+        if (func(arr[i], i, arr)) {
             arr1.push(arr[i])
         }
     }
     return arr1
 }
+
+const reject = (arr, func) => {
+    let arr1 = []
+    for (let i = 0; i < arr.length; i ++) {
+        if (!(func(arr[i], i, arr))) {
+            arr1.push(arr[i])
+        }
+    }
+}
+
