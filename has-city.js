c79c8d1f50e48ccde42da62543ad97e9321f1cdc
jkuya <kuyajesee@proton.me>
2024-09-05 18:51:50 +0300

    Fix: compare the lower case of the passed string to the lowercase of the array ellements

--- has-city.js
+++ has-city.js
@@ -1,7 +1,7 @@
 const hasCity = (country, arr) => {
     return function(city) {
         for (let i = 0; i < arr.length; i++) {
-            if (arr[i] === city) {
+            if (arr[i].toLowerCase() === city.toLowerCase()) {
                 return city + ' is a city from ' + country
             }
             return city + ' is not a city from ' + country
