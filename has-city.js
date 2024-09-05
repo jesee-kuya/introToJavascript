5e3ed486e4e54a60d0fd41e5ea39a8848eb03825
jkuya <kuyajesee@proton.me>
2024-09-05 18:55:42 +0300

    Fix: use indexOf to check if the city is in the array. if it return -1, then the city is not present in the array

--- has-city.js
+++ has-city.js
@@ -1,10 +1,10 @@
 const hasCity = (country, arr) => {
     return function(city) {
         for (let i = 0; i < arr.length; i++) {
-            if (arr[i].toLowerCase() === city.toLowerCase()) {
-                return city + ' is a city from ' + country
+            if (arr.indexOf(city) === -1) {
+                return city + ' is not a city from ' + country
             }
-            return city + ' is not a city from ' + country
+            return city + ' is a city from ' + country
         }
     }
 }
