38450db7d8b7fe5f4745e30ba9b5021ee88384aa
jkuya <kuyajesee@proton.me>
2024-09-05 18:39:18 +0300

    Feat: declare a currying function that adds three numbers

--- sweet-curry.js
+++ sweet-curry.js
@@ -3,3 +3,12 @@ const mult2 = (a) => {
         return a * b
     }
 }
+
+const add3 = (a) => {
+    return function(b) {
+        return function(c) {
+            return a + b + c
+        }
+    }
+}
+
