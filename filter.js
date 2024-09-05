d9cbc7b18f9d7721af3d02c612b35a600830310b
jkuya <kuyajesee@proton.me>
2024-09-05 17:05:35 +0300

    Feat: declare a function that works as [].filter method

---
+++ filter.js
@@ -0,0 +1,9 @@
+const filter = (arr, func) => {
+    let arr1 = []
+    for (let i = 0 ; i < arr.length; i++) {
+        if (func(arr[i, i, arr])) {
+            arr1.push(arr[i])
+        }
+    }
+    return arr1
+}
