99997fd4a6fc87ab9dc876d402da450efd86306d
jkuya <kuyajesee@proton.me>
2024-09-06 17:18:44 +0300

    Fix: select all and not just the first instance

--- get-them-all.js
+++ get-them-all.js
@@ -1,29 +1,30 @@
 const getArchitects = () => {
     return [
-        document.querySelector('body a'),
-        document.querySelector('body span'),
+        document.querySelectorAll('body a'),
+        document.querySelectorAll('body span'),
     ]
 }
 
 const getClassical = () => {
     return [
-        document.querySelector('a.classical'),
-        document.querySelector('a:not(.classical)'),
+        document.querySelectorAll('a.classical'),
+        document.querySelectorAll('a:not(.classical)'),
     ]
 }
 
 const getActive = () => {
     return [
-        document.querySelector('a.classical.active'),
-        document.querySelector('a.classical:not(.active)'),
+        document.querySelectorAll('a.classical.active'),
+        document.querySelectorAll('a.classical:not(.active)'),
     ]
 }
 
 const getBonannoPisano = () => {
     return [
         document.getElementById('BonannoPisano'),
-        document.querySelectorAll('body a'),
+        document.querySelectorAll('a.classical.active'),
     ]
 }
 
 export {getArchitects, getClassical, getActive, getBonannoPisano}
+
