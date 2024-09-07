fe6f4220e62f9d62313e6ef461bbab9d95bc7044
jkuya <kuyajesee@proton.me>
2024-09-07 19:36:57 +0300

    Feat: declare a function pimp which alters the style of a button

---
+++ pimp-my-style.js
@@ -0,0 +1,42 @@
+const styles = [
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen'
+  ];
+  let currentIndex = -1;
+  let isRemoving = false;
+  export function pimp() {
+    const button = document.querySelector('.button');
+    
+    if (!isRemoving) {
+      if (currentIndex < styles.length - 1) {
+        currentIndex++;
+        button.classList.add(styles[currentIndex]);
+      } 
+      if (currentIndex === styles.length - 1) {
+        isRemoving = true;
+        button.classList.add('unpimp');
+      }
+    } else {
+      if (currentIndex >= 0) {
+        button.classList.remove(styles[currentIndex]);
+        currentIndex--;
+      }
+      if (currentIndex === -1) {
+        isRemoving = false;
+        button.classList.remove('unpimp');
+      }
+    }
+  }
