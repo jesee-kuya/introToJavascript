471342a7c90cd5ec53c7c343fab8062338602733
jkuya <kuyajesee@proton.me>
2024-09-07 20:22:09 +0300

    Feat: declare a function generateClasses that generates one class for each color in colors

--- fifty-shades-of-cold.js
+++ fifty-shades-of-cold.js
@@ -1,3 +1,12 @@
 import { colors } from "./fifty-shades-of-cold.data.js";
 
+export const generateClasses = () => {
+    const head = document.getElementsByTagName("head")[0];
+    const style = document.createElement("style");
+    colors.forEach((color) => {
+        style.innerHTML += `.${color} {\n  background: ${color};\n }\n\n`;
+    });
+    console.log(style.innerHTML);
+    head.appendChild(style);
+}
 
