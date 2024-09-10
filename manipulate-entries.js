f0ff76b7344f51a0598ae8fde905ef105614c047
jkuya <kuyajesee@proton.me>
2024-09-10 18:25:57 +0300

    Feat: declare a function cartTotal that will give the details of items in the grocery cart

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -49,3 +49,16 @@ const lowCarbs = (entries) => {
     return filterEntries(entries, (entry) => {let value = (nutritionDB[entry[0]]["carbs"] / 100) * entry[1];return parseInt(value) <= 50;});
 }
 
+const cartTotal = (entries) => {
+    let res = {};
+    for (let key in entries) {
+        res[key] = {};
+        for (let dbKey in nutritionDB[key]) {
+            res[key][dbKey] =
+                Math.round(
+                    (entries[key] / 100) * nutritionDB[key][dbKey] * 1000
+                ) / 1000;
+        }
+    }
+    return res;
+}
