bc4411c032d21f111cc9b9676e647f013b80d82a
jkuya <kuyajesee@proton.me>
2024-09-10 17:25:46 +0300

    Feat: declare a function mapKeys that takes an object and a function. mapkeys passes all the keys in the object through the function and returns a new object with the new keys

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -8,3 +8,14 @@ const filterKeys = (obj, func) => {
     }
     return n
 }
+
+const mapKeys = (obj, func) => {
+    let n = {}
+
+    for (let key in obj) {
+        let k = key
+        key = func(key)
+        n[key] = obj[k]
+    }
+    return n
+}
