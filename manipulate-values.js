1fcf2e3fc41422b08857341e93044248b4d459d8
jkuya <kuyajesee@proton.me>
2024-09-10 17:00:23 +0300

    Feat: declare a function mapValues that takes an object and an operation, the function passes each element of the object through the operation and returns the values

--- manipulate-values.js
+++ manipulate-values.js
@@ -7,3 +7,15 @@ const filterValues = (obj, func) => {
     }
     return n
 }
+
+const mapValues = (obj, func) => {
+    let n = {}
+
+    for (let key in obj) {
+        n[key] = func(obj[key])
+    }
+    return n
+}
+
+const nutrients = { carbohydrates: 12, protein: 20, fat: 5 }
+console.log(mapValues(nutrients, (v) => v+1))
