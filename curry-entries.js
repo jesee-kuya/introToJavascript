13a925584f93b2b96c63420456b90dbb11f01f0d
jkuya <kuyajesee@proton.me>
2024-09-11 11:32:40 +0300

    Fix: the func returns an array, handle the array at index 0

--- curry-entries.js
+++ curry-entries.js
@@ -8,8 +8,8 @@ const mapCurry = (func) => {
     return function(obj1) {
         let n = {}
         for (let key in obj1) {
-            let k, v = func(key, obj1[key]);
-            n[k] = v
+            let k = func(key, obj1[key])
+            n[k[0]] = obj1[key]
         }
         return n
     }
