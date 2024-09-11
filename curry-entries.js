e06a171a58ae438b16f4b70de8e0b1a56254ebcb
jkuya <kuyajesee@proton.me>
2024-09-11 11:23:08 +0300

    Feat: declare a currying function mapCurry that replicates function .map for an object

--- curry-entries.js
+++ curry-entries.js
@@ -3,3 +3,14 @@ const defaultCurry = (obj1) => {
         return {...obj1, ...obj2}
     }
 }
+
+const mapCurry = (func) => {
+    return function(obj1) {
+        let n = {}
+        for (let key in obj1) {
+            let k, v = func(key, obj1[key]);
+            n[k] = v
+        }
+        return n
+    }
+}
