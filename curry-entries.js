c15f74f7b5ce51eb0664149cc9ae784d12e6fc93
jkuya <kuyajesee@proton.me>
2024-09-11 11:40:59 +0300

    Fix: the func takes an array of the value and key, not the individual values

--- curry-entries.js
+++ curry-entries.js
@@ -8,7 +8,7 @@ const mapCurry = (func) => {
     return function(obj1) {
         let n = {}
         for (let key in obj1) {
-            let k = func(key, obj1[key])
+            let k = func([key, obj1[key]])
             n[k[0]] = obj1[key]
         }
         return n
