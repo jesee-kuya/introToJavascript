b102129401e40f56b972cfcc379df8f6c5f46ad1
jkuya <kuyajesee@proton.me>
2024-09-14 14:43:46 +0300

    Feat: declare a currying function opThrottle that handles leading and trailing edges of the throttle interval.

--- throttle.js
+++ throttle.js
@@ -8,3 +8,31 @@ const throttle = (func, wait) => {
         }
     }   
 }
+
+const opThrottle = (func, wait, { leading = false, trailing = true } = {}) => {
+    let last = 0
+    let timer = null
+
+    return function(...args) {
+        const now = Date.now();
+
+        if (!last && !leading) {
+            last = now
+        }
+
+        if (now - last > wait) {
+            if (timer) {
+                clearTimeout(timer);
+                timer = null;
+            }
+            func.apply(this, args);
+            last = now
+        }else if (!timer && trailing) {
+            timer = setTimeout(() => {
+                func.apply(this, args);
+                last = +new Date();
+                timer = null
+            }, wait);
+        }
+    }
+}
