e4f8a3f1035bd9b6098265a0f430f9ec9586861c
jkuya <kuyajesee@proton.me>
2024-09-14 15:38:16 +0300

    Feat: declare a function currying function timeout that executes a callback but will impose a time limit on how long the callback can run.

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -12,3 +12,21 @@ const retry = (count = 3, callback = async () => {}) => {
         }
     }
 }
+
+const timeout = (delay = 0, callback = async () => {}) => {   
+    return async function(...args) {
+        const timeout = new Promise((resolve) =>
+            setTimeout(resolve, delay, Error('timeout'))
+        );
+        const functionCall = new Promise((resolve) =>
+            resolve(callback(...args))
+        );
+        const res = await Promise.race([timeout, functionCall]).then(
+            (res) => res
+        );
+        if (res instanceof Error) {
+            throw res
+        }
+        return res
+    };
+}
