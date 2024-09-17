4e970721d07b6179db157c988f4e164b4a1b3651
jkuya <kuyajesee@proton.me>
2024-09-17 18:31:57 +0300

    Docs: Create a tell-me-vip.mjs script that filters the guests who actually answered 'YES' to your invitation, and save this list in a vip.txt file

---
+++ tell-me-vip.mjs
@@ -0,0 +1,38 @@
+import { readdir, writeFile,readFile } from "node:fs/promises";
+import path from "path";
+
+let arg = process.argv[2];
+if (arg === undefined) {
+  arg = process.argv[1];
+}
+
+try {
+  const data = await readdir(arg, "utf8");
+  let arr = [];
+  for (let i = 0; i < data.length; i++) {
+    if (await saidYes(path.join(arg, data[i]))) {
+      let s = data[i].replace(/\.json$/, "");
+      let [lastName, firstName] = s.split("_");
+      arr.push(`${firstName} ${lastName}`);
+    }
+  }
+  arr.sort()
+
+  let s = arr.map((name, index) => `${index + 1}. ${name}`).join("\n");
+
+  await writeFile("vip.txt", s);
+  console.log(s);
+} catch (err) {
+  console.error("Error:", err);
+}
+
+async function saidYes(filename) {
+  try {
+    const data = await readFile(filename, "utf8");
+    const jsonData = JSON.parse(data);
+    return jsonData.answer === "yes";
+  } catch (err) {
+    console.error("Error reading/parsing file:", filename, err);
+    return false;
+  }
+}
